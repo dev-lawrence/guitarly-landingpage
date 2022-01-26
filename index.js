4934de1eab8b69905a4c4759cf804877c73899c6
dev-lawrence <lawrenceamrasakpare19@gmail.com>
2022-01-26 23:19:01 +0100

    finished the testimonial section

--- index.js
+++ index.js
@@ -7,8 +7,7 @@ const icon = menuBtn.querySelector('i');
 const testiCards = document.querySelectorAll('.testimonial__card');
 const indicators = document.querySelector('.indicators');
 const dots = Array.from(indicators.children);
-
-let activeSlide = 1;
+let activeCard = 1;
 
 // add bg color to the header on scroll
 const stickOnScroll = () => {
@@ -39,23 +38,72 @@ navLinks.forEach((link) => {
 // manual navigation
 const manualNav = (manual) => {
   testiCards[manual].classList.add('current');
-  dots[manual].classList.add('current');
+  dots[manual].classList.add('active');
 };
 
+indicators.addEventListener('click', (e) => {
+  const targetDot = e.target.closest('button');
+
+  if (!targetDot) return;
+  const targetIndex = dots.findIndex((dot) => dot === targetDot);
+  // get the current card and current dot
+  const current = document.querySelector('.current');
+  const currentDot = document.querySelector('.active');
+  // remove the current class from the current card
+  current.classList.remove('current');
+  // remove the currentDot class from the current dot
+  currentDot.classList.remove('active');
+  // add the current class to the next card
+  manualNav(targetIndex);
+  activeCard = targetIndex;
+});
+
 // move card on when each dot is clicked
-dots.forEach((dot, index) => {
-  dot.addEventListener('click', () => {
-    //    get the current card
+// const dotNav = (dot, index) => {
+//   dot.addEventListener('click', () => {
+//     //    get the current card and current dot
+//     const current = document.querySelector('.current');
+//     const currentDot = document.querySelector('.active');
+//     // remove the current class from the current card
+//     current.classList.remove('current');
+//     // remove the currentDot class from the current dot
+//     currentDot.classList.remove('active');
+//     // add the current class to the next card
+//     manualNav(index);
+//     activeCard = index;
+
+//     const targetDot = dots.findIndex((d) => d === dotNav);
+//     console.log(targetDot);
+//   });
+// };
+
+// dots.forEach(dotNav);
+
+// autoplay navigation
+
+let repeat = () => {
+  setInterval(() => {
     const current = document.querySelector('.current');
+    const currentDot = document.querySelector('.active');
     // remove the current class from the current card
     current.classList.remove('current');
-    // add the current class to the next card
-    // testiCards[index].classList.add('current');
-    // activeSlide = index;
-    manualNav(index);
-    activeSlide = index;
-  });
-});
+    // remove the currentDot class from the current dot
+    currentDot.classList.remove('active');
+    testiCards[activeCard].classList.add('current');
+    dots[activeCard].classList.add('active');
+    activeCard++;
+
+    if (testiCards.length == activeCard) {
+      activeCard = 0;
+    }
+
+    if (activeCard >= testiCards.length) {
+      return;
+    }
+  }, 7000);
+};
+
+repeat();
 
 // event listeners
 window.addEventListener('scroll', stickOnScroll);
